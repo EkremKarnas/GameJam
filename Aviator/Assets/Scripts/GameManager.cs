787585e2640fdc341ae807e5fc10c3a471f25110
Akin Capar <akincapar2@gmail.com>
2022-01-08 21:48:44 +0300

    game manager changes

--- Aviator/Assets/Scripts/GameManager.cs
+++ Aviator/Assets/Scripts/GameManager.cs
@@ -10,13 +10,17 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    public int betAmount = 0;
-    private int roundCountDown;
+    private float roundCountDown;
 
     [Header("Texts")]
     [SerializeField] private TextMeshProUGUI roundCountdownText;
     [SerializeField] private TextMeshProUGUI multiplierText;
-     
+    [SerializeField] private TextMeshProUGUI betAmountText;
+    [SerializeField] private TextMeshProUGUI bankAmountText;
+
+    [Header("Images")]
+    [SerializeField] private Image betButtonImage;
+    [SerializeField] private Image cashOutButtonImage;
 
     [Header("Buttons")]
     [SerializeField] private GameObject fiftyDollarsButton;
@@ -27,11 +31,14 @@ public class GameManager : MonoBehaviour
 
     [Header("Variables")]
     [SerializeField] private float multiplier = 1f;
-    [SerializeField] private float sessionTime;
-    [SerializeField] private float betMoney = 0f;
+    [SerializeField] private float sessionMultiplier;
+    [SerializeField] private float betMoney;
+    [SerializeField] private float bettedMoney;
     [SerializeField] private float bankMoney = 10000f;
 
+
     [Header("Booleans")]
+    [SerializeField] private bool canBet = true;
 
     [SerializeField]
 
@@ -41,58 +48,64 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
-
-        Time.timeScale = 0f;
     }
 
 
     private void Start()
     {
         roundCountDown = 10;
-        StartCoroutine(FirstCountDown());
+        canBet = true;
     }
 
-    IEnumerator FirstCountDown()
+    private void CountDown()
     {
-        if (roundCountDown > 0)
+        if (roundCountDown <= 0)
         {
-            roundCountDown--;
-            yield return new WaitForSeconds(1);
-        }
-
-        if(roundCountDown == 0)
-        {
-            StopCoroutine(FirstCountDown());
+            roundCountdownText.gameObject.SetActive(false);
             StartRound();
         }
+
+        roundCountDown -= 1 * Time.deltaTime;
+        roundCountdownText.text = Mathf.RoundToInt(roundCountDown).ToString();
     }
 
     private void Update()
     {
-        
+        betAmountText.text = betMoney.ToString();
+        bankAmountText.text = bankMoney.ToString();
+        multiplierText.text = multiplier.ToString() + "x";
+        CountDown();
+
     }
 
     private void StartRound()
     {
-        Time.timeScale = 1;
-        fiftyDollarsButton.GetComponent<Button>().enabled = false;
-        hundredDollarsButton.GetComponent<Button>().enabled = false;
-        fiveHundredDollarsButton.GetComponent<Button>().enabled = false;
+        canBet = false;
+        sessionMultiplier = UnityEngine.Random.Range(1f, 30f);
+        //fiftyDollarsButton.GetComponent<Button>().enabled = false;
+        //hundredDollarsButton.GetComponent<Button>().enabled = false;
+        //fiveHundredDollarsButton.GetComponent<Button>().enabled = false;
         betButton.SetActive(false);
+        betButtonImage.gameObject.SetActive(false);
         cashOutButton.SetActive(true);
+        cashOutButtonImage.gameObject.SetActive(true);
         StartMultiplier();
 
     }
 
     private void StartMultiplier()
     {
-        multiplier += Time.deltaTime / 5;
-        betMoney = betMoney * multiplier;
+        if (sessionMultiplier > multiplier)
+        {
+            multiplier += Time.deltaTime / 5;
+            multiplier = Mathf.Round(multiplier * 1000.0f) * 0.001f;
+            betMoney = bettedMoney * multiplier;
+        }
     }
 
     public void bet50Dollars()
     {
-        if (bankMoney - betMoney >= 50)
+        if (bankMoney - betMoney >= 50 && canBet == true)
         {
             betMoney += 50;
         }
@@ -100,7 +113,7 @@ public class GameManager : MonoBehaviour
 
     public void bet100Dollars()
     {
-        if (bankMoney - betMoney >= 100)
+        if (bankMoney - betMoney >= 100 && canBet == true)
         {
             betMoney += 100;
         }
@@ -108,7 +121,7 @@ public class GameManager : MonoBehaviour
 
     public void bet500Dollars()
     {
-        if (bankMoney - betMoney >= 500)
+        if (bankMoney - betMoney >= 500 && canBet == true)
         {
             betMoney += 500;
         }
@@ -116,6 +129,11 @@ public class GameManager : MonoBehaviour
 
     public void betTotalBetMoney()
     {
-        bankMoney -= betMoney;
+        if (canBet == true)
+        {
+            bankMoney -= betMoney;
+            bettedMoney = betMoney;
+        }
+        canBet = false;
     }
 }
